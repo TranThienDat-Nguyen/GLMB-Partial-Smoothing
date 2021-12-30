6d1f882727c3b99ad84225e53fb7a75dcd591592
Tran Thien Dat Nguyen <66776548+TranThienDat-Nguyen@users.noreply.github.com>
2021-12-30 16:06:38 +0800

    Update run_filter.m

--- jointglmb_smooth/ukf/run_filter.m
+++ jointglmb_smooth/ukf/run_filter.m
@@ -1,25 +1,37 @@
 function [est, est_smooth] = run_filter(model,meas)
-
-% This is the MATLAB code for the implementation of the Generalized Labeled Multi-Bernoulli filter proposed in
+% This is the demo script for the (joint prediction and update) implementation of the Generalized Labeled Multi-Bernoulli filter 
+% and the partial smoothing technique proposed in
+% T.T.D. Nguyen and D.Y. Kim, "GLMB Tracker with Partial Smoothing," Sensors, Vol. 19, No. 20, pp. 4419, 2019. 
+% https://https://www.mdpi.com/1424-8220/19/20/4419
 % B.-T. Vo, and B.-N. Vo, "An Efficient Implementation of the Generalized Labeled Multi-Bernoulli Filter," IEEE Trans. Signal Processing, Vol. 65, No. 8, pp. 1975-1987, 2017.
 % http://ba-ngu.vo-au.com/vo/VVH_FastGLMB_TSP17.pdf
-% based on the original filter proposed in
-% B.-T. Vo, and B.-N. Vo, "Labeled Random Finite Sets and Multi-Object Conjugate Priors," IEEE Trans. Signal Processing, Vol. 61, No. 13, pp. 3460-3475, 2013.
-% http://ba-ngu.vo-au.com/vo/VV_Conjugate_TSP13.pdf
-% and via Murty's algorithm as per
+% corresponding to the (separate prediction and update) implementation proposed in
 % B.-N. Vo, B.-T. Vo, and D. Phung, "Labeled Random Finite Sets and the Bayes Multi-Target Tracking Filter," IEEE Trans. Signal Processing, Vol. 62, No. 24, pp. 6554-6567, 2014
 % http://ba-ngu.vo-au.com/vo/VVP_GLMB_TSP14.pdf
+% of the original filter proposed in
+% B.-T. Vo, and B.-N. Vo, "Labeled Random Finite Sets and Multi-Object Conjugate Priors," IEEE Trans. Signal Processing, Vol. 61, No. 13, pp. 3460-3475, 2013.
+% http://ba-ngu.vo-au.com/vo/VV_Conjugate_TSP13.pdf
 %
 % ---BibTeX entry
-% @ARTICLE{GLMB1,
-% author={B.-T. Vo and B.-N. Vo
+% @ARTICLE{GLMB_PartialSmooth,
+%   author    = {Nguyen, Tran Thien Dat and Kim, Du Yong},
+%   title     = {GLMB Tracker with Partial Smoothing},
+%   journal   = {Sensors},
+%   year      = {2019},
+%   volume    = {19},
+%   number    = {20},
+%   pages     = {4419},
+%   publisher = {Multidisciplinary Digital Publishing Institute}}
+%
+% @ARTICLE{GLMB3,
+% author={B.-N. Vo and B.-T. Vo and H. Hung},
 % journal={IEEE Transactions on Signal Processing},
-% title={Labeled Random Finite Sets and Multi-Object Conjugate Priors},
-% year={2013},
-% month={Jul}
-% volume={61},
-% number={13},
-% pages={3460-3475}}
+% title={An Efficient Implementation of the Generalized Labeled Multi-Bernoulli Filter},
+% year={2017},
+% month={Apr}
+% volume={65},
+% number={8},
+% pages={1975-1987}}
 %
 % @ARTICLE{GLMB2,
 % author={B.-T. Vo and B.-N. Vo and D. Phung},
@@ -31,15 +43,15 @@ function [est, est_smooth] = run_filter(model,meas)
 % number={24},
 % pages={6554-6567}}
 %
-% @ARTICLE{GLMB3,
-% author={B.-N. Vo and B.-T. Vo and H. Hung},
+% @ARTICLE{GLMB1,
+% author={B.-T. Vo and B.-N. Vo
 % journal={IEEE Transactions on Signal Processing},
-% title={An Efficient Implementation of the Generalized Labeled Multi-Bernoulli Filter},
-% year={2017},
-% month={Apr}
-% volume={65},
-% number={8},
-% pages={1975-1987}}
+% title={Labeled Random Finite Sets and Multi-Object Conjugate Priors},
+% year={2013},
+% month={Jul}
+% volume={61},
+% number={13},
+% pages={3460-3475}}
 %---
 
 %=== Setup
